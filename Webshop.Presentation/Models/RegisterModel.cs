58c0db1cce61b89bc51d760b189e2ba44bf5458f
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-04-04 17:49:06 +0200

    Register page (#14)
    * Add register page, can register user
    
    * Add register button on login page

---
+++ Webshop.Presentation/Models/RegisterModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webshop.Presentation.Models;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "Förnamn är obligatoriskt")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Efternamn är obligatoriskt")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "E-post är obligatoriskt")]
+    [EmailAddress(ErrorMessage = "Ogiltig e-postadress")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Gatuadress är obligatoriskt")]
+    public string StreetAdress { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Postnummer är obligatoriskt")]
+    [Range(10000, 99999, ErrorMessage = "Postnummer måste vara 5 siffror")]
+    public int PostalCode { get; set; }
+
+    [Required(ErrorMessage = "Stad är obligatoriskt")]
+    public string City { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Lösenord är obligatoriskt")]
+    [MinLength(6, ErrorMessage = "Lösenordet måste vara minst 6 tecken")]
+    public string Password { get; set; } = string.Empty;
+
+    [Compare(nameof(Password), ErrorMessage = "Lösenorden matchar inte")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
---
+++ Webshop.Presentation/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Webshop.Application.DTOs.Responses;
+using Webshop.Application.DTOs.UserDTOs;
+using Webshop.Presentation.Mapping;
+using Webshop.Presentation.Models;
+
+namespace Webshop.Presentation.Services;
+
+public class UserService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public UserService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<bool> RegisterAsync(RegisterModel model)
+    {
+        var request = model.MapToRequest();
+
+        var client = _httpClientFactory.CreateClient("API");
+        var response = await client.PostAsJsonAsync<RegisterUserRequest>("api/auth/register", request);
+
+        return response.IsSuccessStatusCode ? true : false;
+    }
+
+}
